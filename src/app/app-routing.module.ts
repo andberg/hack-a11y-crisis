770393d90b5fcf21ddd719698023b9efa47fc7eb
Andrea Berglund <andreaberglund@outlook.com>
2022-05-08 16:31:36 +0200

    skapade bas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BadComponent } from './pages/bad/bad.component';
+import { GoodComponent } from './pages/good/good.component';
+import { StartComponent } from './pages/start/start.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: StartComponent },
+  { path: 'bad', component: BadComponent },
+  { path: 'good', component: GoodComponent },
+  { path: '*', redirectTo: '', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BadComponent } from './pages/bad/bad.component';
+import { GoodComponent } from './pages/good/good.component';
+import { StartComponent } from './pages/start/start.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    GoodComponent,
+    BadComponent,
+    StartComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
